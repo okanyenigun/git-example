5193ceefea8c69253af196338fc6bd181a866752
okanyenigun <okanyenigun@gmail.com>
2026-01-06 14:50:24 +0300

    feat: add configurable suffix to Greeter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 class Greeter:
-    def __init__(self, prefix: str = "Hello"):
+    def __init__(self, prefix: str = "Hello", suffix: str = "!"):
         self.prefix = prefix
+        self.suffix = suffix
 
     def greet(self, name: str) -> str:
-        return f"{self.prefix}, {name}!"
+        return f"{self.prefix}, {name}{self.suffix}"
